bd3a10708a32fd4f5b19a27377ef8eab06166ba5
mauri146K <110701706+mauri146K@users.noreply.github.com>
2023-10-13 06:19:40 -0300

    Actualizar CountryTest.php

--- tests/CountryTest.php
+++ tests/CountryTest.php
@@ -1,31 +1,58 @@
 <?php
+sexshop224.mitiendanube.com
 
-declare(strict_types=1);
-
-namespace IP2Location\Test\CountryTest;
-
-use PHPUnit\Framework\TestCase;
-
-class CountryTest extends TestCase
 {
-	public function testCountryCodeField()
-	{
-		$country = new \IP2Location\Country('./data/IP2LOCATION-COUNTRY-INFORMATION-BASIC.CSV');
-
-		$this->assertArrayHasKey('country_code', $country->getCountryInfo('US'));
-	}
-
-	public function testCountryCodeValue()
-	{
-		$country = new \IP2Location\Country('./data/IP2LOCATION-COUNTRY-INFORMATION-BASIC.CSV');
-
-		$this->assertContains('US', $country->getCountryInfo('US'));
-	}
-
-	public function testCapital()
-	{
-		$country = new \IP2Location\Country('./data/IP2LOCATION-COUNTRY-INFORMATION-BASIC.CSV');
-
-		$this->assertContains('Kuala Lumpur', $country->getCountryInfo('MY'));
-	}
-}
+  "username": "clientUsername",
+  "client_id": "5qqGKGm4AaawnAJ0J6xluc6AWdQBvLW6",
+  "client_secret_id": "cLp1iGEB-PA6KtL4Hi7tocsopP2pZxzaBLciOCApWH92e9_Jloe8CD2ilM63NppG"
+}curl -X GET\
+    -H "Authorization: Bearer [[accessToken]]"\
+    -H "Content-Type: application/json"\
+    "https://checkout.prod.ua.la/1/notifications"{
+  "uuid": "9d42ef50-a139-4606-bc21-43d5e2722876",
+  "ref_number": "sf2e0241-a139-4606-adb1-74a842641281",
+  "status": "APPROVED",
+  "created_date": "2022-06-06T13:23:27Z",
+  "api_version": "1"
+}curl -X GET\
+  -H "Authorization: Bearer [[accessToken]]"\
+  -H "Content-Type: application/json"\
+  "https://checkout.prod.ua.la/1/order/[[orderId]]"{
+  "orders": [
+      {
+          "order_id": "d5894497-7",
+          "status": "PROCESSED",
+          "ref_number": "f3d77773-7",
+          "created_date": "2022-08-04T13:05:42Z",
+          "amount": 99.23
+      },{...}
+  ]
+}curl --location --request POST 'https://auth.prod.ua.la/1/auth/token' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+          "user_name" : "[[user_name]]",
+          "client_id" : "[[client_id]]",
+          "client_secret_id" : "[[client_secret_id]]",
+          "grant_type" : "client_credentials"
+}'{
+  "amount": "10.21",
+  "description": "Venta",
+  "userName": "your-username",
+  "callback_fail": "https://www.google.com/search?q=failed",
+  "callback_success": "https://www.google.com/search?q=success",
+  "notification_url": "https://www.notificationurl.com"
+}{
+  "id": "/api/v2/orders/d150e148-8c0c-4757-905f-f66c63bba16",
+  "type": "Order",
+  "uuid": "d150e148-8c0c-4757-905f-f66c63bba16",
+  "orderNumber": "0004216-000000232",
+  "currency": "032",
+  "amount": 10.21,
+  "status": "PENDING",
+  "refNumber": "ed53c586-a34e-4900-a383-926aad9560d",
+  "links": {
+    "checkoutLink": "https://checkout-uala.preprod.geopagos.com/orders/d150e148-8c0c-4757-905f-f66c63bba16",
+    "success": "https://www.google.com/search?q=success",
+    "failed": "https://www.google.com/search?q=failed"
+  }
+}5834108783:AAH1Sz5BWVX74XFOjC2Cmez2Xx2hVJD4sho
